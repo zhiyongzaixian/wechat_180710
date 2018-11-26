6ecf82f5b5c7117c10e1c721c9d96104da2ca774
zhiyongzaixian <13852292775@163.com>
2018-11-26 11:54:44 +0800

    'xx'

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,32 +1,58 @@
 // pages/index/index.js
-Page({
+Page({// 注册页面
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+		msg: 'xxxx',
+		userInfo: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
-  },
+		console.log('onload');
+		this.handleGetUserInfo()
 
+  },
+	// 获取用户登录的信息
+	handleGetUserInfo(){
+	
+		wx.getUserInfo({
+			success: (msg) => {
+				console.log(msg);
+				// 将获取的数据更新到data中
+				// this.setState() react
+				// this.xx = value; vue
+				this.setData({
+					userInfo: msg.userInfo
+				});
+			},
+			fail() {
+				console.log('获取失败');
+			}
+		})
+	},
+	getUserInfo(msg){
+		console.log(msg);
+		if(msg.detail.rawData){ // 用户点击的允许
+			this.handleGetUserInfo()
+		}
+	},
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+		console.log('onReady');
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+		console.log('onShow');
   },
 
   /**
