04ac45fefe27451bc5e19ff07939ca1498c54fe1
zhiyongzaixian <13852292775@163.com>
2018-11-27 10:32:34 +0800

    'xx'

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 App({ // 注册整个应用
+	data: { // 缓存某一个页面音乐播放的状态
+		isPlay: false,
+		pageIndex: null
+	},
 
 	/**
 	 * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,5 +1,7 @@
 // pages/detail/detail.js
 let listData = require('../../datas/list-data.js');
+let appData = getApp();
+console.log(appData.data.msg);
 Page({
 
   /**
@@ -7,7 +9,8 @@ Page({
    */
   data: {
 		detailObj: {},
-		isCollected: false // 默认未收藏
+		isCollected: false, // 默认未收藏
+		isMusicPlay: false // 默认未播放
   },
 
   /**
@@ -30,6 +33,33 @@ Page({
 				isCollected: true
 			})
 		}
+
+		// 判断当前页面音乐是否在播放
+		if (appData.data.isPlay && appData.data.pageIndex === index){
+			// 更新状态
+			this.setData({
+				isMusicPlay: true
+			})
+		}
+
+		// 监听音乐播放和暂停
+		wx.onBackgroundAudioPlay(() => {
+			console.log('音乐播放');
+			this.setData({
+				isMusicPlay: true
+			});
+
+			appData.data.isPlay = true;
+			appData.data.pageIndex = index;
+		})
+
+		wx.onBackgroundAudioPause(() => {
+			console.log('音乐暂停');
+			this.setData({
+				isMusicPlay: false
+			});
+			appData.data.isPlay = false;
+		 })
   },
 	// 处理收藏功能的函数
 	handleCollection(){
@@ -55,6 +85,24 @@ Page({
 			key: 'isCollected',
 			data: obj
 		})
+	},
+	// 处理音乐播放的功能函数
+	handleMusicPlay(){
+		let isMusicPlay = !this.data.isMusicPlay;
+		this.setData({
+			isMusicPlay
+		});
+
+		// 处理音乐播放和暂停
+		let { dataUrl, title} = this.data.detailObj.music
+		if (isMusicPlay){ // 播放
+			wx.playBackgroundAudio({
+				dataUrl,
+				title
+			})
+		}else {  // 暂停
+			wx.pauseBackgroundAudio()
+		}
 	},
 
   /**
